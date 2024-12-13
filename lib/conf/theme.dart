71fc800dfa7ce56a90a897d5f69fc3320ec0c7a7
Yogeshwar Pawade <yogeshwar.pawade@gmail.com>
2024-12-13 15:29:25 -0500

    modular/theme

--- lib/conf/theme.dart
+++ lib/conf/theme.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
 
-// Dark theme colors
+// Define Dark Theme Colors
 final Color darkBackgroundColor = Color(0xFF080833);
 final Color darkPrimaryColor = Color(0xFF4A2DB3);
 final Color darkTextColor = Colors.white;
-
-// Light theme colors
-final Color lightBackgroundColor = Color(0xFFF0F0F0);
-final Color lightPrimaryColor = Color(0xFF1E88E5);
-final Color lightTextColor = Colors.black;
 
 // Dark Theme Data
 final ThemeData darkTheme = ThemeData(
@@ -24,20 +19,8 @@ final ThemeData darkTheme = ThemeData(
     buttonColor: darkPrimaryColor,
     textTheme: ButtonTextTheme.primary,
   ),
-);
-
-// Light Theme Data
-final ThemeData lightTheme = ThemeData(
-  brightness: Brightness.light,
-  primaryColor: lightPrimaryColor,
-  scaffoldBackgroundColor: lightBackgroundColor,
-  textTheme: TextTheme(),
-  appBarTheme: AppBarTheme(
-    backgroundColor: lightPrimaryColor,
-    iconTheme: IconThemeData(color: lightTextColor),
-  ),
-  buttonTheme: ButtonThemeData(
-    buttonColor: lightPrimaryColor,
-    textTheme: ButtonTextTheme.primary,
+  floatingActionButtonTheme: FloatingActionButtonThemeData(
+    backgroundColor: darkPrimaryColor,
+    foregroundColor: darkTextColor,
   ),
 );
